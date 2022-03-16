f58439f961f70731a6e1882821a5d24e88ae05c8
lexteezy <lexterr235@gmail.com>
2022-03-16 13:46:08 +0800

    add oto needs

--- src/App.js
+++ src/App.js
@@ -4,272 +4,226 @@ import strongNodeAbi from "./strongNodeAbi.json";
 import playmateNodeAbi from "./playmateNodeAbi.json";
 import thorNodeAbi from "./thorNodeAbi.json";
 import nodacAbi from "./nodacAbi.json";
+import wavaxAbi from "./wavaxAbi.json"
 import { ethers } from "ethers";
 import { useState, useEffect } from 'react';
+import Axios from 'axios';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Oto from './Pages/Oto';
+import Base from './Pages/Base';
 
 
 function App() {
-  const [strongNodeCount, setStrongNodeCount] = useState(0);
-  const [playmatesNodeCount, setPlaymatesNodeCount] = useState(0);
-  const [thorNodeCount, setThorNodeCount] = useState(0);
-  const [userTotalEarnings, setUserTotalEarnings] = useState({
-    realizedGains: 0,
-    unpaidEarnings: 0
-  });
-  const [userFTMTotalEarnings, setUserFTMTotalEarnings] = useState({
-    realizedGains: 0,
-    unpaidEarnings: 0
-  });
-  const [userAVAXTotalEarnings, setUserAVAXTotalEarnings] = useState({
-    realizedGains: 0,
-    unpaidEarnings: 0
-  });
-  const [count, setCount] = useState(0);
-
-  let strongNodeContract;
-  let playmateNodeContract;
-  let thorNodeContract;
-
-  let signer;
-  let signerAddy;
-
-
-
-  const marketingWallet = "0xEd56a7F78b830518ff00808e2bAff0F4bDc722Ed";
-
-
-  useEffect(() => {
-    // const provider = new ethers.providers.Web3Provider(window.ethereum);
-    const ethProvider = new ethers.providers.getDefaultProvider(1);
-    strongNodeContract = new ethers.Contract(
-      "0xFbdDaDD80fe7bda00B901FbAf73803F2238Ae655",
-      strongNodeAbi,
-      ethProvider
-    );
-    const avaxProvider = new ethers.providers.getDefaultProvider("https://api.avax.network/ext/bc/C/rpc");
-    playmateNodeContract = new ethers.Contract(
-      "0xc4a25F823582d9ccf5cf8C8BF5338073e7a51676",
-      playmateNodeAbi,
-      avaxProvider
-    );
-    thorNodeContract = new ethers.Contract(
-      "0x2be139283d73Cd3a335A94c50BA2B28fdE1E06E1",
-      thorNodeAbi,
-      avaxProvider
-    );
-
-    // const ftmProvider = new ethers.providers.getDefaultProvider("https://rpcapi.fantom.network");
-
-    // nodacContract = new ethers.Contract(
-    //   "0x803e78269f7F013b7D13ba13243Be10C66418a70",
-    //   nodacAbi,
-    //   ftmProvider
-    // );
-
-
-    getStrongNodeCount();
-    getPlaymatesNodeCount();
-    getThorNodeCount();
-    if(signerAddy) {
-      getDynamicUserTotalEarnings("https://rpcapi.fantom.network", "FTM");
-      getDynamicUserTotalEarnings("https://api.avax.network/ext/bc/C/rpc", "AVAX");
-    }
-    setCount(count + 1);
-  }, [count === 1]);
-
-
-  const getStrongNodeCount = async () => {
-    // const provider = new ethers.providers.Web3Provider(window.ethereum);
-    // const provider = new ethers.providers.getDefaultProvider(1);
-    const strongNodeCount = await strongNodeContract.entityNodeCount(marketingWallet);
-
-    setStrongNodeCount(String(strongNodeCount));
-  }
-
-  const getPlaymatesNodeCount = async () => {
-    const playmatesNodeCount = await playmateNodeContract.balanceOf(marketingWallet);
-
-    setPlaymatesNodeCount(String(playmatesNodeCount));
-  }
-
-  const getThorNodeCount = async () => {
-    const thorNodeCount = await thorNodeContract.getNodeNumberOf(marketingWallet);
-
-    setThorNodeCount(String(thorNodeCount));
-  }
-
-  const conenctWallet = async() => {
-    console.log("connect Wallet");
-    await window.ethereum.request({ method: 'eth_requestAccounts' });
-    const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
-    await provider.send("eth_requestAccounts", []);
-    signer = await provider.getSigner();
-    signerAddy = await signer.getAddress();
-    getDynamicUserTotalEarnings("https://rpcapi.fantom.network", "FTM");
-    getDynamicUserTotalEarnings("https://api.avax.network/ext/bc/C/rpc", "AVAX");
-  }
-
-  const getUserTotalEarnings = async() => {
-    const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
-    await provider.send("eth_requestAccounts", []);
-    console.log(provider);
-    const nodac = new ethers.Contract(
-      "0x803e78269f7F013b7D13ba13243Be10C66418a70",
-      nodacAbi,
-      provider
-    );
-    const signer = await provider.getSigner();
-    const signerAddress = await signer.getAddress();
-    const totalEarnings = await nodac.getUserRealizedGains(signerAddress);
-    const unpaidEarnings = await nodac.getUserUnpaidEarnings(signerAddress);
-
-    setUserTotalEarnings({
-      realizedGains: String(ethers.utils.formatUnits(totalEarnings, 18)),
-      unpaidEarnings: String(ethers.utils.formatUnits(unpaidEarnings, 18))
-    });
-    
-  }
 
   
-  const getDynamicUserTotalEarnings = async(providerRpc, network) => {
-    await window.ethereum.request({ method: 'eth_requestAccounts' });
-    const dynamicProvider = new ethers.providers.getDefaultProvider(providerRpc);
-    //provider to get address from metamask
-    const nodac = new ethers.Contract(
-      "0x803e78269f7F013b7D13ba13243Be10C66418a70",
-      nodacAbi,
-      dynamicProvider
-    );
-    const totalEarnings = ethers.utils.formatUnits(await nodac.getUserRealizedGains(signerAddy));
-    const unpaidEarnings = ethers.utils.formatUnits(await nodac.getUserUnpaidEarnings(signerAddy));
-    const simplifiedTotEarns = (Math.round(totalEarnings * 100) / 100).toFixed(2);
-    const simplifiedUnpaidEarns = (Math.round(unpaidEarnings * 100) / 100).toFixed(2);
-    if(network === "FTM") {
-      setUserFTMTotalEarnings({
-        realizedGains: String(simplifiedTotEarns),
-        unpaidEarnings: String(simplifiedUnpaidEarns)
-      });
-    } else if(network === "AVAX") {
-      setUserAVAXTotalEarnings({
-        realizedGains: String(simplifiedTotEarns),
-        unpaidEarnings: String(simplifiedUnpaidEarns)
-      });
-    }
-    
-  }
+//   const [strongNodeCount, setStrongNodeCount] = useState(0);
+//   const [playmatesNodeCount, setPlaymatesNodeCount] = useState(0);
+//   const [thorNodeCount, setThorNodeCount] = useState(0);
+//   const [userFTMTotalEarnings, setUserFTMTotalEarnings] = useState({
+//     balance: 0,
+//     realizedGains: 0,
+//     unpaidEarnings: 0
+//   });
+//   const [userAVAXTotalEarnings, setUserAVAXTotalEarnings] = useState({
+//     balance: 0,
+//     realizedGains: 0,
+//     unpaidEarnings: 0
+//   });
+//   const [nodacPrice, setNodacPrice] = useState(0);
+//   const [count, setCount] = useState(0);
+//   const [days, setDays] = useState(0);
+//   let state ={value: ''};
 
-  const giveMeWelfarePlease = async() => {
-    const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
-    console.log(provider);
-    await provider.send("eth_requestAccounts", []);
-    const signer = await provider.getSigner();
-    const nodac = new ethers.Contract(
-      "0x803e78269f7F013b7D13ba13243Be10C66418a70",
-      nodacAbi,
-      signer
-    );
-    await nodac.giveMeWelfarePlease();
+//   let strongNodeContract;
+//   let playmateNodeContract;
+//   let thorNodeContract;
+//   let wavaxContract;
+//   let nodacContract;
 
-    getUserTotalEarnings();
-  }
+//   let signer;
+//   let signerAddy;
+//   let avaxPrice;
+// //6dd32a74-9045-42f3-8796-02bb32573fbe API KEY
+
+
+//   const marketingWallet = "0xEd56a7F78b830518ff00808e2bAff0F4bDc722Ed";
+
+
+//   useEffect(() => {
+//     // const provider = new ethers.providers.Web3Provider(window.ethereum);
+//     Axios.get("https://api.coinstats.app/public/v1/coins/avalanche-2").then(
+//       (response) => {
+//         console.log(response.data);
+//         avaxPrice = response.data.coin.price;
+//         console.log('avaxPrice ', avaxPrice);
+//       }
+//     );
+//     const ethProvider = new ethers.providers.getDefaultProvider(1);
+//     strongNodeContract = new ethers.Contract(
+//       "0xFbdDaDD80fe7bda00B901FbAf73803F2238Ae655",
+//       strongNodeAbi,
+//       ethProvider
+//     );
+//     const avaxProvider = new ethers.providers.getDefaultProvider("https://api.avax.network/ext/bc/C/rpc");
+//     playmateNodeContract = new ethers.Contract(
+//       "0xc4a25F823582d9ccf5cf8C8BF5338073e7a51676",
+//       playmateNodeAbi,
+//       avaxProvider
+//     );
+//     thorNodeContract = new ethers.Contract(
+//       "0x2be139283d73Cd3a335A94c50BA2B28fdE1E06E1",
+//       thorNodeAbi,
+//       avaxProvider
+//     );
+//     wavaxContract = new ethers.Contract(
+//       "0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7",
+//       wavaxAbi,
+//       avaxProvider
+//     );
+//     nodacContract = new ethers.Contract(
+//       "0x803e78269f7F013b7D13ba13243Be10C66418a70",
+//       nodacAbi,
+//       avaxProvider
+//     );
+
+//     // const ftmProvider = new ethers.providers.getDefaultProvider("https://rpcapi.fantom.network");
+
+//     // nodacContract = new ethers.Contract(
+//     //   "0x803e78269f7F013b7D13ba13243Be10C66418a70",
+//     //   nodacAbi,
+//     //   ftmProvider
+//     // );
+
+
+//     getStrongNodeCount();
+//     getPlaymatesNodeCount();
+//     getThorNodeCount();
+//     if(signerAddy) {
+//       getDynamicUserTotalEarnings("https://rpcapi.fantom.network", "FTM");
+//       getDynamicUserTotalEarnings("https://api.avax.network/ext/bc/C/rpc", "AVAX");
+//     }
+//     getNodacPrice();
+//     setCount(count + 1);
+//   }, [count === 1]);
+
+
+//   const getStrongNodeCount = async () => {
+//     // const provider = new ethers.providers.Web3Provider(window.ethereum);
+//     // const provider = new ethers.providers.getDefaultProvider(1);
+//     const strongNodeCount = await strongNodeContract.entityNodeCount(marketingWallet);
+
+//     setStrongNodeCount(String(strongNodeCount));
+//   }
+
+//   const getPlaymatesNodeCount = async () => {
+//     const playmatesNodeCount = await playmateNodeContract.balanceOf(marketingWallet);
+
+//     setPlaymatesNodeCount(String(playmatesNodeCount));
+//   }
+
+//   const getThorNodeCount = async () => {
+//     const thorNodeCount = await thorNodeContract.getNodeNumberOf(marketingWallet);
+
+//     setThorNodeCount(String(thorNodeCount));
+//   }
+
+//   const conenctWallet = async() => {
+//     console.log("connect Wallet");
+//     await window.ethereum.request({ method: 'eth_requestAccounts' });
+//     const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+//     await provider.send("eth_requestAccounts", []);
+//     signer = await provider.getSigner();
+//     signerAddy = await signer.getAddress();
+//     getDynamicUserTotalEarnings("https://rpcapi.fantom.network", "FTM");
+//     getDynamicUserTotalEarnings("https://api.avax.network/ext/bc/C/rpc", "AVAX");
+//   }
   
+//   const getNodacPrice = async() => {
+//     const avaxBalance = ethers.utils.formatUnits(await wavaxContract.balanceOf("0xE175bd0B421331BfA06E619b77A022Ec98899aC3"), 18);
+//     const nodacBalance = ethers.utils.formatUnits(await nodacContract.balanceOf("0xE175bd0B421331BfA06E619b77A022Ec98899aC3"), 18);
+//     console.log("avaxBalance ", avaxBalance, 18);
+//     console.log("nodacBalance ", nodacBalance, 18);
+//     const avaxBalanceInUsd = avaxBalance * avaxPrice;
+//     console.log(avaxPrice);
+//     console.log("avaxInUsd ", avaxBalanceInUsd);
+//     const nodacPrice = avaxBalanceInUsd / nodacBalance;
+//     console.log(nodacPrice);
+//     setNodacPrice(nodacPrice);
+//   }
+
+  
+//   const getDynamicUserTotalEarnings = async(providerRpc, network) => {
+//     await window.ethereum.request({ method: 'eth_requestAccounts' });
+//     const dynamicProvider = new ethers.providers.getDefaultProvider(providerRpc);
+//     //provider to get address from metamask
+//     const nodac = new ethers.Contract(
+//       "0x803e78269f7F013b7D13ba13243Be10C66418a70",
+//       nodacAbi,
+//       dynamicProvider
+//     );
+//     const balance = ethers.utils.formatUnits(await nodac.balanceOf(signerAddy));
+//     const totalEarnings = ethers.utils.formatUnits(await nodac.getUserRealizedGains(signerAddy));
+//     const unpaidEarnings = ethers.utils.formatUnits(await nodac.getUserUnpaidEarnings(signerAddy));
+//     const simplifiedBalance = (Math.round(balance * 100) / 100).toFixed(2);
+//     const simplifiedTotEarns = (Math.round(totalEarnings * 100) / 100).toFixed(2);
+//     const simplifiedUnpaidEarns = (Math.round(unpaidEarnings * 100) / 100).toFixed(2);
+//     if(network === "FTM") {
+//       setUserFTMTotalEarnings({
+//         balance: String(simplifiedBalance),
+//         realizedGains: String(simplifiedTotEarns),
+//         unpaidEarnings: String(simplifiedUnpaidEarns)
+//       });
+//     } else if(network === "AVAX") {
+//       setUserAVAXTotalEarnings({
+//         balance: String(simplifiedBalance),
+//         realizedGains: String(simplifiedTotEarns),
+//         unpaidEarnings: String(simplifiedUnpaidEarns)
+//       });
+//     }
+    
+//   }
+
+//   // const calculateRewards = async (event) => {
+//   //   //enter days value
+//   //   const format =  1;
+//   //   console.log('format',format);
+//   //   const rebaseRate = 0.02362;
+//   //   const rebaseTimePerDay = (60 * 24) / 15;
+//   //   const nextYield = rebaseRate / 100;
+//   //   console.log(nextYield);
+//   //   let tokenAmount = format;
+//   //   const originalTokenAmount = tokenAmount;
+//   //   console.log('rebaseRate', rebaseRate);
+//   //   console.log('rebaseTimePerDay', rebaseTimePerDay);
+//   //   tokenAmount = tokenAmount * Math.pow((1+nextYield),96 * days);
+//   //   console.log(tokenAmount);
+//   //   tokenAmount = Number(tokenAmount).toFixed(2);
+//   //   // tokenAmount -= originalTokenAmount;
+//   //   console.log('originalTokenAmount', originalTokenAmount);
+//   //   console.log('tokenAmountAfter', days,'days: ', tokenAmount);
+//   // }
+
+//   const giveMeWelfarePlease = async() => {
+//     const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+//     console.log(provider);
+//     await provider.send("eth_requestAccounts", []);
+//     const signer = await provider.getSigner();
+//     const nodac = new ethers.Contract(
+//       "0x803e78269f7F013b7D13ba13243Be10C66418a70",
+//       nodacAbi,
+//       signer
+//     );
+//     await nodac.giveMeWelfarePlease();
+
+//   }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <button onClick={conenctWallet}
-        type="submit">
-          Connect Wallet
-        </button>
-        <p>
-          Strong Node Count is {strongNodeCount}
-        </p>
-        <p>
-          Playmates Node Count is {playmatesNodeCount}
-        </p>
-        <p>
-          Thor Node Count is {thorNodeCount}
-        </p>
-
-        <button onClick={getUserTotalEarnings}
-        type="submit">
-          Get User Total Earnings
-        </button>
-        <table>
-          <thead>
-            <tr>
-              <th>Total Earnings</th>
-              <th>Unrealized Gains</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>
-                {userTotalEarnings.realizedGains}
-              </td>
-              <td>
-                {userTotalEarnings.unpaidEarnings}
-              </td>
-            </tr>
-          </tbody>
-        </table>
-        <button onClick={giveMeWelfarePlease}
-        type="submit">
-          Claim Dividend
-        </button>
-        <table>
-          <thead>
-            <tr>
-              <th rowSpan={2}>FTM</th>
-            </tr>
-            <tr>
-              <th>Total Earnings</th>
-              <th>Unrealized Gains</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>
-                {userFTMTotalEarnings.realizedGains}
-              </td>
-              <td>
-                {userFTMTotalEarnings.unpaidEarnings}
-              </td>
-            </tr>
-          </tbody>
-        </table>
-        <table>
-          <thead>
-            <tr>
-              <th rowSpan={2}>AVAX</th>
-            </tr>
-            <tr>
-              <th>Total Earnings</th>
-              <th>Unrealized Gains</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>
-                {userAVAXTotalEarnings.realizedGains}
-              </td>
-              <td>
-                {userAVAXTotalEarnings.unpaidEarnings}
-              </td>
-            </tr>
-          </tbody>
-        </table>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+    <Routes>
+      <Route path="/base" element={<Base/>}/>
+      <Route path="/oto" element={<Oto/>}/>
+    </Routes>
+    </Router>
   );
 }
 
---
+++ src/Pages/Oto.js
@@ -0,0 +1,186 @@
+import saforkAbi from '../ABI/saforkAbi.json';
+import wavaxAbi from "../wavaxAbi.json"
+import { useState, useEffect } from 'react';
+import Axios from 'axios';
+import { ethers } from 'ethers';
+
+/* TODO: NEED TO FIX SOME RACE CONDITIONS BETWEEN PROMISES TO ACHIEVE EXPECTED/CONSISTENT OUTPUTS */
+function Oto() {
+
+    const [tokenPrice, setTokenPrice] = useState(0);
+    const [tokenDecimal, setTokenDecimal] = useState(5);
+    const [tokenSupply, setTokenSupply] = useState({
+        totalSupply: 0,
+        circulatingSupply: 0,
+        firepitPercentage: 0
+    });
+    const [taxReceiverBalances, setTaxReceiverBalances] = useState({
+        treasury: 0,
+        insurance: 0,
+        firepit: 0
+    });
+    const [taxesInUsd, setTaxesInUsd] = useState({
+        treasury: 0,
+        insurance: 0,
+        firepit: 0
+    });
+    const [count, setCount] = useState(0);
+
+    const avaxProvider = new ethers.providers.getDefaultProvider("https://api.avax.network/ext/bc/C/rpc");
+
+    const firepitAddress = "0x4DaDa77b4497718b0D543CdC0434b6aFb651842F"; //REPLACE WITH ACTUAL TOKEN FIREPIT CA
+    const treasuryAddress = "0x7d1516e604C53627Ea8596D09e71BFe3239A4461"; //REPLACE WITH ACTUAL TOKEN TREASURY CA
+    const insuranceAddress = "0x4336383Dbd4099D7F7bB31A02308ADbfFe5B74F1"; //REPLACE WITH ACTUAL TOKEN INSURANCE CA
+
+    const lpPair = "0xe2E8289f1F1Bc395116f2b50d59F41cF2Cda9b4f"; //REPLACE WITH ACTUAL OTO's LP PAIR CA
+
+    let tokenContract;
+    let wavaxContract;
+    let avaxPrice;
+
+    useEffect(() => {
+        Axios.get("https://api.coinstats.app/public/v1/coins/avalanche-2").then(
+          (response) => {
+            avaxPrice = response.data.coin.price;
+          }
+        );
+        wavaxContract = new ethers.Contract(
+            "0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7",
+            wavaxAbi,
+            avaxProvider
+        );
+        tokenContract = new ethers.Contract(
+            "0xb0c20a934c9dc89bdf1280fde0f3122c2ebb0fb9", //REPLACE WITH OTO's CA
+            saforkAbi,
+            avaxProvider
+        );
+
+        getTokenDecimals();
+        getTokenPrice();
+        setTimeout(() => "", 5000);
+        getTaxReceiverBalances();
+        getTotalSupply();
+    }, [wavaxContract, tokenContract, avaxPrice]);
+
+    const getTokenDecimals = async() => {
+        console.log('getTokenDecimals');
+        const tokenDecimal = await tokenContract._decimals();
+        setTokenDecimal(tokenDecimal);
+    }
+
+    const getTotalSupply = async() => {
+        console.log('getTotalSupply');
+        const totalSupply = ethers.utils.formatUnits(await tokenContract._totalSupply(), 5);
+        const firepitSupply = ethers.utils.formatUnits(await tokenContract.balanceOf(firepitAddress), tokenDecimal);
+        console.log('firepitSupply', firepitSupply);
+        const firepitPercentage = ((firepitSupply / totalSupply) * 100).toFixed(2);
+        setTokenSupply({
+            totalSupply: totalSupply,
+            circulatingSupply: totalSupply - firepitSupply,
+            firepitPercentage: firepitPercentage
+        });
+    }
+
+    const getTaxReceiverBalances = async() => {
+        console.log('getTaxReceiverBalances');
+        const firepitBalance =   ethers.utils.formatUnits(await tokenContract.balanceOf(firepitAddress), tokenDecimal);
+        const treasuryBalance =   ethers.utils.formatUnits(await tokenContract.balanceOf(treasuryAddress), tokenDecimal);
+        const insuranceBalance =   ethers.utils.formatUnits(await tokenContract.balanceOf(insuranceAddress), tokenDecimal);
+        setTimeout(() => "", 5000);
+        //NEED TO FIX RACE CONDITION ^^^^^^^^ 
+        let firepitInUsd;
+        if(firepitBalance) {
+            console.log('firepitBalance', firepitBalance);
+            firepitInUsd = await getTokenInUsd(firepitAddress);
+        }
+        const treasuryInUsd = await getTokenInUsd(treasuryAddress);
+        const insuranceInUsd = await getTokenInUsd(insuranceAddress);
+
+        setTaxReceiverBalances({
+            firepit: firepitBalance,
+            treasury: treasuryBalance,
+            insurance: insuranceBalance
+        });
+        setTaxesInUsd({
+            firepit: firepitInUsd,
+            treasury: treasuryInUsd,
+            insurance: insuranceInUsd
+        });
+    }
+    
+    const getTokenInUsd = async(address) => {
+        console.log('getTokenInUsd');
+        const tokenBalance = ethers.utils.formatUnits(await tokenContract.balanceOf(address), tokenDecimal);
+        setTimeout(() => "",  5000);
+        const tokenInUsd = tokenBalance * tokenPrice;
+        return tokenInUsd;
+    }
+      
+    //GET TOKEN PRICE WITHOUT COINGECKO/CMC ONLY BASED ON LIQUIDITY PAIR
+    const getTokenPrice = async() => {
+      console.log('getTokenPrice');
+      const avaxBalance = ethers.utils.formatUnits(await wavaxContract.balanceOf(lpPair), 18);
+      const tokenBalance = ethers.utils.formatUnits(await tokenContract.balanceOf(lpPair), tokenDecimal);
+      const avaxBalanceInUsd = avaxBalance * avaxPrice;
+      const tPrice = (avaxBalanceInUsd / tokenBalance).toFixed(5);
+      setTokenPrice(tPrice);
+    }
+
+    return (
+        <div className="Oto">
+            <header className="App-header">
+                Hello World
+                <table>
+                    <thead>
+                        <tr aria-colspan={2}></tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>TokenPrice</td>
+                            <td>{tokenPrice}</td>
+                        </tr>
+                        <tr>
+                            <td>TotalSupply</td>
+                            <td>{tokenSupply.totalSupply}</td>
+                        </tr>
+                        <tr>
+                            <td>CirculatingSupply</td>
+                            <td>{tokenSupply.circulatingSupply}</td>
+                        </tr>
+                        <tr>
+                            <td>Treasury</td>
+                            <td>{taxReceiverBalances.treasury}</td>
+                        </tr>
+                        <tr>
+                            <td>Treasury IN USD</td>
+                            <td>{taxesInUsd.treasury}</td>
+                        </tr>
+                        <tr>
+                            <td>Insurance</td>
+                            <td>{taxReceiverBalances.insurance}</td>
+                        </tr>
+                        <tr>
+                            <td>Insurance IN USD</td>
+                            <td>{taxesInUsd.insurance}</td>
+                        </tr>
+                        <tr>
+                            <td>Firepit</td>
+                            <td>{taxReceiverBalances.firepit}</td> 
+                        </tr>
+                        <tr>
+                            <td>Firepit IN USD</td>
+                            <td>{taxesInUsd.firepit}</td>
+                        </tr>
+                        <tr>
+                            <td>% FirePit Supply</td>
+                            <td>{tokenSupply.firepitPercentage}%</td>
+                        </tr>
+                    </tbody>
+                </table>
+                
+            </header>
+        </div>
+    );    
+}
+
+export default Oto;
